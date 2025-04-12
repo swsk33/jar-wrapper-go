d5cbdae486ef44b9ff4f8d2b0c0fdff4091b28dd
守望时空33 <yanhanhui2001a@163.com>
2025-04-12 11:51:29 +0800

    升级依赖，修复有时候可能打包失败的问题

--- builder/cmd/version.go
+++ builder/cmd/version.go
@@ -12,7 +12,7 @@ var versionCmd = &cobra.Command{
 	Short: "输出版本号",
 	Long:  "用于输出当前的版本号，x.y.z格式",
 	Run: func(cmd *cobra.Command, args []string) {
-		color.HiGreen("jar2exe-go version %d.%d.%d", 1, 3, 0)
+		color.HiGreen("jar2exe-go version %d.%d.%d", 1, 3, 1)
 		color.HiBlue("jar-wrapper-go builder by swsk33")
 		color.HiYellow("Build using %s", runtime.Version())
 	},
